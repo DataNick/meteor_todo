7f5002cab598271f57d41103b4aa35028b83743c
DataNick <DataNick@users.noreply.github.com>
2015-09-05 00:05:39 -0400

    Added ability to add items by submitting form value

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,28 @@ if (Meteor.isClient) {
 // this code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      console.log(event);
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.word.value;
+
+      // Insert task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.word.value = ""
+
     }
   });
 }
